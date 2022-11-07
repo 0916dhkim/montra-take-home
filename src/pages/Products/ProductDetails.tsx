2248143393eafb0c55fb2ed02cbe7e30dfff17ab
Donghyeon Kim <0916dhkim@gmail.com>
2022-11-06 23:20:22 -0500

    Make add to cart UI functional

--- src/pages/Products/ProductDetails.tsx
+++ src/pages/Products/ProductDetails.tsx
@@ -1,9 +1,14 @@
+import { ChangeEventHandler, useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "../../store/store";
+import { useNavigate, useParams } from "react-router-dom";
+
+import { addToCart } from "../../store/cart";
 import classes from "./ProductDetails.module.css";
-import { useAppSelector } from "../../store/store";
-import { useParams } from "react-router-dom";
 
 export default function ProductDetails() {
   const { id } = useParams();
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
   const product = useAppSelector((state) => {
     const found = state.products.find((each) => each.id.toString() === id);
     if (found == null) {
@@ -11,6 +16,39 @@ export default function ProductDetails() {
     }
     return found;
   });
+
+  const [quantityInput, setQuantityInput] = useState("1");
+  const [error, setError] = useState<string | null>(null);
+
+  const validateQuantity = (): number | null => {
+    const value = Number(quantityInput);
+    if (isNaN(value)) {
+      setError("Invalid quantity.");
+      return null;
+    }
+    if (value <= 0) {
+      setError("Quantity should be greater than 0.");
+      return null;
+    }
+    setError(null);
+    return value;
+  };
+
+  useEffect(() => {
+    validateQuantity();
+  }, [quantityInput]);
+
+  const handleQuantityChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setQuantityInput(e.target.value);
+  };
+
+  const handleAdd = () => {
+    const quantity = validateQuantity();
+    if (quantity) {
+      dispatch(addToCart({ product, quantity }));
+      navigate("/cart");
+    }
+  };
 
   return (
     <div className={classes.detailsGrid}>
@@ -23,9 +61,17 @@ export default function ProductDetails() {
         <img src={product.thumbnail} className={classes.productImage} />
         <p>Price: ${product.price}</p>
         <p>
-          Quantity: <input className={classes.quantityInput} />
+          Quantity:{" "}
+          <input
+            className={classes.quantityInput}
+            value={quantityInput}
+            onChange={handleQuantityChange}
+          />
         </p>
-        <button className={classes.addButton}>Add to Cart</button>
+        {error ? <p className={classes.errorLabel}>{error}</p> : null}
+        <button className={classes.addButton} onClick={() => handleAdd()}>
+          Add to Cart
+        </button>
       </div>
     </div>
   );
